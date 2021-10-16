5f391178aa83160039b4091f4ca4e43dbff75698
Joao-Pedro-Lopes <joaopedrolopes00@gmail.com>
2021-10-16 22:52:26 +0100

    correction write storage function

--- Project/DIDAProject2122/LibDIDAStorage/DIDAStorageOperations.cs
+++ Project/DIDAProject2122/LibDIDAStorage/DIDAStorageOperations.cs
@@ -35,7 +35,7 @@ namespace LibDIDAStorage
 
         DIDAVersion IDIDAStorage.write(string id, string val)
         {
-            int latestVersionNumber = 0;
+            int latestVersionNumber = -1;
             int replicaId = 0; //tenho de saber em q replica quero meter -- should be a list or a dict
 
             foreach (DIDARecord r in recordsList)
