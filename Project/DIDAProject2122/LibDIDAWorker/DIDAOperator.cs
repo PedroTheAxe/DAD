074fe286b71506976c75df5f4012e3f93a12691e
Joao-Pedro-Lopes <joaopedrolopes00@gmail.com>
2021-10-16 19:36:24 +0100

    grpc e proto mal

---
+++ Project/DIDAProject2122/LibDIDAWorker/DIDAOperator.cs
@@ -0,0 +1,22 @@
+﻿using DIDAWorker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+//using DIDAStorageClient;
+
+namespace LibDIDAWorker
+{
+    class DIDAOperator : IDIDAOperator
+    {
+        //Dictionary<string, DIDAStorageService.DIDAStorageServiceClient> _storageServers = new Dictionary<string, DIDAStorageService.DIDAStorageServiceClient>();
+        public void ConfigureStorage(DIDAStorageNode[] storageReplicas, delLocateStorageId locationFunction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ProcessRecord(DIDAMetaRecord meta, string input, string previousOperatorOutput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
